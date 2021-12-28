3ab702de30774e4b1691f69bf67cf4b6f4f93bf2
Villermen <villermen@gmail.com>
2021-12-28 14:22:54 +0100

    Remove old code in index.php

--- public/index.php
+++ public/index.php
@@ -10,24 +10,3 @@ $app = new App();
 $response = $app->run($request);
 $response->prepare($request);
 $response->send();
-
-exit();
-
-
-require_once("minecraft/modules/mysql.php");
-require_once("minecraft/modules/misc.php");
-require_once("minecraft/modules/account.php");
-
-//banner background
-$bannerBackground = false;
-//$bannerBackground = "img/banners/onmate_vacguy99_creativechristmas.png"; //override
-
-if (!$bannerBackground)
-{
-	if ($bannerBackgrounds = glob("img/banners/*.png"))
-	{
-		$bannerBackground = $bannerBackgrounds[rand(0, count($bannerBackgrounds) - 1)];
-		$bannerBackground = "img/banners/" . basename($bannerBackground);
-	}
-}
-?>
