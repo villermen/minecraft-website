5929b026566c4772e02cb5655ae19ab38e955761
Villermen <villermen@gmail.com>
2021-12-28 14:32:02 +0100

    Force christmas banner automatically

--- src/Service/TwigExtensions.php
+++ src/Service/TwigExtensions.php
@@ -31,9 +31,16 @@ class TwigExtensions
 
     public function randomBannerFunction(): string
     {
-        $bannerGlob = $this->config->getProjectRoot() . '/public/img/banner/*.png';
-        $banners = glob($bannerGlob);
-        $banner = $banners[mt_rand(0, count($banners) - 1)];
-        return $this->pathFunction('/img/banner/' . basename($banner));
+        $banner = null;
+        $monthDay = (int)date('nd');
+        if ($monthDay >= 1220 || $monthDay <= 107) {
+            $banner = 'onmate_vacguy99_creativechristmas.png';
+        } else {
+            $bannerGlob = sprintf('%s/public/img/banner/*.png', $this->config->getProjectRoot());
+            $banners = glob($bannerGlob);
+            $banner = basename($banners[mt_rand(0, count($banners) - 1)]);
+        }
+
+        return $this->pathFunction(sprintf('/img/banner/%s', $banner));
     }
 }
